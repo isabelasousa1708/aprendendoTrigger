4fdf980bde58f36aaf1d214424c98ab792aa6b25
isabelasousa1708 <67762414+isabelasousa1708@users.noreply.github.com>
2021-04-16 20:04:55 -0300

    Add files via upload

---
+++ trigger.sql
@@ -0,0 +1,70 @@
+DROP TABLE profissionais;
+DROP TABLE pacientes;
+DROP TABLE especialidades;
+DROP TABLE consultas;
+DROP TABLE obitos;
+
+create table  pacientes(
+  id serial PRIMARY KEY,
+  nome varchar(40) not null,
+  sexo varchar(20),
+  obito boolean
+);
+
+create table profissionais(
+  id serial PRIMARY KEY,
+  nome varchar(50)
+);
+
+create table especialidades(
+  id serial,
+  nome varchar(50)
+);
+
+create table consultas(
+  id serial PRIMARY KEY,
+  especialidade_id integer,
+  profiss_id integer
+);
+
+create table  obitos(
+  id serial PRIMARY KEY,
+  bs text
+);
+
+--ALTERANDO TABELAS ADICIONANDO CONSULTAS
+ALTER TABLE consultas ADD CONSTRAINT FkEspecialidadeDaConsulta FOREIGN KEY
+    (especialidade_id)
+REFERENCES especialidades(id);
+
+ALTER TABLE consultas ADD CONSTRAINT FkProfissionalDaConsulta FOREIGN KEY   
+    (profiss_id)
+REFERENCES profissionais(id);
+
+--CRINDO A FUNÇÃO DA TRIGGER
+CREATE OR REPLACE FUNCTION trgValidaDadosConsulta()  RETURNS trigger AS $trgValidaDadosConsulta$ --$ = ASPAS
+DECLARE
+  id_pac int;
+  dados_do_paciente record;
+  dados_do_especialidade record;
+  BEGIN
+     -- caso seja um registro novo ele mostra antes de addicionar na tabela
+     RAISE NOTICE 'NOVO REGISTRO: id = ', NEW.id;
+     RAISE NOTICE 'NOVO REGISTRO: especialidade_id = %', NEW.especialidade_id;
+     RAISE NOTICE 'NOVO REGISTRO: profiss_id = ', NEW.profiss_id;
+
+        IF NEW.especialidade_id = 2 THEN 
+         RAISE EXCEPTION 'Nao estamos aceitando INSERT id = 2';
+        END IF;
+    
+    RETURN NEW;
+END;
+
+$trgValidaDadosConsulta$ LANGUAGE plpgsql; 
+
+CREATE TRIGGER ValidaDadosConsulta
+BEFORE INSERT OR UPDATE ON consultas
+FOR EACH ROW --FOR EACH STATEMENT
+ EXECUTE PROCEDURE trgValidaDadosConsulta();
+
+ --colocanr drop table, create table
